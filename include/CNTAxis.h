c50f2315ba205ca60f51023a24c23940e8b9f41e
will4ntz <wiliam-m-@hotmail.com>
2025-10-04 16:01:19 -0300

    :truck: code changed place

--- include/CNTAxis.h
+++ include/CNTAxis.h
@@ -1,47 +1,21 @@
-#ifndef UNTITLED_LIBRARY_H
-#define UNTITLED_LIBRARY_H
+#ifndef CNTAXIS_LIBRARY_H
+#define CNTAXIS_LIBRARY_H
 
 #include <onnxruntime/onnxruntime_cxx_api.h>
 #include <opencv2/opencv.hpp>
 #include "../meta/types.h"
 #include <vector>
+#include "ONNXModel.h"
 
-
-
-class CNTAxis {
+class CNTAxis final : public ONNXModel {
 public:
-    explicit CNTAxis(const std::string& model_path);
-
+    explicit CNTAxis(const std::string &model_path);
     explicit CNTAxis(const unsigned char model_weights[], const unsigned int model_weights_size);
 
-    virtual ~CNTAxis();
+    ~CNTAxis() override;
 
-    float* infer(const std::vector<float>& input_data);
-
-    std::vector<Detection> postProcess(float* input_data) const;
-
-    virtual void WarmUpSession();
-
-protected:
-    Ort::Session session_;
-    Ort::Env env_;
-    Ort::MemoryInfo memory_info_;
-
-    std::vector<int64_t> input_shape_;
-    std::vector<int64_t> output_shape_;
-
-    std::vector<const char*> input_node_names_;
-    std::vector<const char*> output_node_names_;
-
-    std::vector<std::string> classes {};
-
-    float confThreshold = 0.1;
-    float iouThreshold = 0.5;
-
-    void initializeIO();
-
-    Ort::Value createTensor(const std::vector<float>& data, const std::vector<int64_t>& dimensions) const;
+    void setClasses(const std::vector<std::string> &classes);
+    std::vector<std::string> getClasses() const;
 };
 
-
-#endif // UNTITLED_LIBRARY_H
+#endif // CNTAXIS_LIBRARY_H
---
+++ include/ONNXModel.h
@@ -0,0 +1,49 @@
+//
+// Created by willian on 10/4/25.
+//
+
+#ifndef CNTAXIS_ONNXMODEL_H
+#define CNTAXIS_ONNXMODEL_H
+
+#include <onnxruntime/onnxruntime_cxx_api.h>
+#include <opencv2/opencv.hpp>
+#include "../meta/types.h"
+#include <vector>
+
+
+class ONNXModel {
+public:
+    explicit ONNXModel(const std::string& model_path);
+
+    explicit ONNXModel(const unsigned char model_weights[], const unsigned int model_weights_size);
+
+    virtual ~ONNXModel();
+
+    float* infer(const std::vector<float>& input_data);
+
+    std::vector<Detection> postProcess(float* input_data) const;
+
+    virtual void WarmUpSession();
+
+protected:
+    Ort::Session session_;
+    Ort::Env env_;
+    Ort::MemoryInfo memory_info_;
+
+    std::vector<int64_t> input_shape_;
+    std::vector<int64_t> output_shape_;
+
+    std::vector<const char*> input_node_names_;
+    std::vector<const char*> output_node_names_;
+
+    std::vector<std::string> classes {};
+
+    float confThreshold = 0.1;
+    float iouThreshold = 0.5;
+
+    void initializeIO();
+
+    Ort::Value createTensor(const std::vector<float>& data, const std::vector<int64_t>& dimensions) const;
+};
+
+#endif //CNTAXIS_ONNXMODEL_H
