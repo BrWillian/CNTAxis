b3f219d7b355abcd27f200e5408f9bcc20b2185d
will4ntz <wiliam-m-@hotmail.com>
2025-10-04 17:18:36 -0300

    :sparkles: added methods CNTAxis class

--- src/CNTAxis.cpp
+++ src/CNTAxis.cpp
@@ -1,7 +1,16 @@
 #include "../include/CNTAxis.h"
 
-#include <iostream>
+CNTAxis::CNTAxis(const std::string &model_path) : ONNXModel(model_path){
+    this->classes = {"vehicle", "axis"};
+}
+CNTAxis::CNTAxis(const unsigned char model_weights[], const unsigned int model_weights_size) : ONNXModel(model_weights, model_weights_size){
+    this->classes = {"vehicle", "axis"};
+}
+CNTAxis::~CNTAxis() = default;
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+void CNTAxis::setClasses(const std::vector<std::string> &classes) {
+    this->classes = classes;
+}
+std::vector<std::string> CNTAxis::getClasses() const {
+    return this->classes;
 }
