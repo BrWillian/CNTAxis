d86d6d49e457e95f626dc3eb3d895e95e1d5bb60
will4ntz <wiliam-m-@hotmail.com>
2025-10-13 07:42:08 -0300

    :memo: updated cmake recipe

---
+++ include/CNTCore.h
@@ -0,0 +1,13 @@
+//
+// Created by willian on 10/5/25.
+//
+
+#ifndef CNTAXIS_CNTCORE_H
+#define CNTAXIS_CNTCORE_H
+
+
+class CNTCore {
+};
+
+
+#endif //CNTAXIS_CNTCORE_H
---
+++ src/CNTCore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by willian on 10/5/25.
+//
+
+#include "../include/CNTCore.h"
