dd0686433137fb349e10b835fe1d5a6d246efd5b
will4ntz <wiliam-m-@hotmail.com>
2026-05-07 15:21:19 -0300

    :sparkles: added macro for generate version and weights

---
+++ meta/Version.h
@@ -0,0 +1,12 @@
+//
+// Created by willian on 10/19/25.
+//
+
+#ifndef CNTAXIS_VERSION_H
+#define CNTAXIS_VERSION_H
+
+#define GIT_COMMIT_HASH         "@GIT_COMMIT_HASH@"
+#define GIT_BRANCH              "@GIT_BRANCH@"
+#define CNTAXIS_VERSION         "@CNTAXIS_VERSION@"
+
+#endif //CNTAXIS_VERSION_H
---
+++ meta/Weights.h
@@ -0,0 +1,10 @@
+//
+// Created by willian on 10/19/25.
+//
+
+#ifndef CNTAXIS_WEIGHTS_H
+#define CNTAXIS_WEIGHTS_H
+
+@CNTAXIS_WEIGHTS@
+
+#endif //CNTAXIS_WEIGHTS_H
